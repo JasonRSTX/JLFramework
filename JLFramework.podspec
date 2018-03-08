2a1d41c47bcddfb44667eb0ee8ba1774c99a02f1
jason <rstx_reg@aliyun.com>
2018-03-08 08:44:05 +0800

    add JDLog for Debug

--- JLFramework.podspec
+++ JLFramework.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'JLFramework'
-  s.version          = '0.1.2'
+  s.version          = '0.1.3'
   s.summary          = 'JLFramwork is a set of coding tools and coding style.'
 
   s.homepage         = 'https://github.com/JasonRSTX/JLFramework'
--- JLFramework/Classes/JLFramework.h
+++ JLFramework/Classes/JLFramework.h
@@ -9,5 +9,13 @@
 #ifndef JLFramework_h
 #define JLFramework_h
 
+#ifdef DEBUG
+#define JDLog(...) NSLog(@"\n{类名}:%s,\n{行数}:%d\n{NSLog}:%@ \n\n", __func__, __LINE__, [NSString stringWithFormat: __VA_ARGS__])
+#else
+#   define JDLog(...) /* */
+#endif
+
+#define JDLogFunc JLDebugLog(@"%s",__func__)
+#define JDLogFuncStr(str) JDLog(@"%s,%@",__func__,str)
 
 #endif /* JLFramework_h */
